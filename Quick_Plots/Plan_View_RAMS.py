dda6791d5418ef6e76fb23702260d7c110db87d4
Peter James Marinescu <pmarines@pfe26.nas.nasa.gov>
2023-03-27 15:03:41 -0700

    Added parallelization of single domain plots

--- Quick_Plots/Plan_View_RAMS.py
+++ Quick_Plots/Plan_View_RAMS.py
@@ -14,23 +14,27 @@ import h5py
 import hdf5plugin
 from collections import OrderedDict
 import math
+from jug import TaskGenerator
 
 # Path to RAMS .h5 and head.txt files
-path = '/nobackup/pmarines/PROD/ARG1.2-R/G2/out/'
+#path = '/nobackup/ldgrant/pmarines/PROD/ARG1.2-R/G3/out_30s/'
+path = '/nobackup/pmarines/PROD/DRC1.1-R/G3-C/out_30s/'
 
 # Path to save plots
-savepath = '/nobackup/pmarines/PROD/ARG1.2-R/G2/out/Plots/'
+#savepath = '/nobackup/ldgrant/pmarines/PROD/ARG1.2-R/G3/out_30s/Plots/'
+savepath = '/nobackup/pmarines/PROD/DRC1.1-R/G3-C/out_30s/Plots/'
 
 # Specify array of variables to plots
 # ****** Make sure to check and make sure variable is present in clvls, cmaps, and scale dictionaries below; If not there add it
-vars = ['RLONTOP','WP','RTP'] # RAMS model variables
+vars = ['VP','UP','WP'] # RAMS model variables
+#vars = ['RTP','RLONTOP']
 
 # Specify altitudes of levels to plots
-lvls = [0,5000,10000,15000] # m
+lvls = [50,5000,10000,15000] # m
 
 # Specify grid number to plot
-gridnum = '2'
-filetype = 'A' # Analysis (A) or Lite (L) files
+gridnum = '3'
+filetype = 'L' # Analysis (A) or Lite (L) files
 
 # Specify grid point bounds for zooming in
 # leave x0 = -1 to not use this feature
@@ -55,10 +59,10 @@ cmaps = OrderedDict()
 cmaps['WP'] = plt.cm.bwr
 cmaps['UP'] = plt.cm.bwr
 cmaps['VP'] = plt.cm.bwr
-cmaps['PCPVR'] = plt.cm.jet
-cmaps['RTP'] = plt.cm.jet
-cmaps['RV'] = plt.cm.jet
-cmaps['RLONTOP'] = plt.cm.jet
+cmaps['PCPVR'] = plt.cm.viridis
+cmaps['RTP'] = plt.cm.viridis
+cmaps['RV'] = plt.cm.viridis
+cmaps['RLONTOP'] = plt.cm.viridis
 
 scale = OrderedDict()
 scale['WP'] = 1 #m/s
@@ -70,13 +74,100 @@ scale['RV'] = 1000 #g/kg
 scale['RLONTOP'] = 1 #W/m2
 
 # Specify details of a potential new domain
-plt_mult_dom = 1; # 1 for plotting multiple available analysis domains, 0 for plotting just specified domain above
-plt_new_dom = 1; # 0 for no, 1 for yes; only works when plt_mult_dom is on
+plt_mult_dom = 0; # 1 for plotting multiple available analysis domains, 0 for plotting just specified domain above
+plt_new_dom = 0; # 0 for no, 1 for yes; only works when plt_mult_dom is on
 lat_p = OrderedDict() ; lon_p = OrderedDict()
-name = 'new'
-cen_lat = 16.4 ; cen_lon = 128.75
-nx = 1250; ny = 950;
+name = 'new'; sname3 = '' # Don't Change these parameters
+cen_lat = -31.75; cen_lon = -64.5
+nx = 2630; ny = 2780;
+dx = 100 ; dy = 100 #m
+
+cen_lat = -4.0; cen_lon = 23.7
+nx = 1424; ny = 1208;
 dx = 400 ; dy = 400 #m
+
+cen_lat = -4.3; cen_lon = 24.9
+nx = 1880; ny = 2670;
+dx = 100 ; dy = 100 #m
+
+#### Define Function for plotting single domain (for parallization)
+@TaskGenerator
+def plot_single_dom(var, znow, hefile, h5file, x0,x1,y0,y1, scale, clvls, cmaps, tlvls, savepath, sname, gridnum):
+
+    # Get RAMS vertical coodinate levels
+    zcoords = np.array(rams_tools.calc_zcoords(hefile))
+    dzcoords = np.diff(zcoords)
+    # Get the vertical coordinate index associated with the specified altitudes
+    zid = np.where(np.abs(zcoords-znow) == np.min(np.abs(zcoords-znow)))[0][0]
+    print(zid)
+
+    # convert altitude to string for filenames
+    lvl = int(np.array(zcoords[zid]))
+    if lvl < 10:
+       lvlstr = '0000'+str(lvl)
+    elif lvl < 100:
+       lvlstr = '000'+str(lvl)
+    elif lvl < 1000:
+       lvlstr = '00'+str(lvl)
+    elif lvl < 10000:
+       lvlstr = '0'+str(lvl)
+    else:
+       lvlstr = str(lvl)
+
+    # Grab current time from rams filename
+    cur_time = os.path.split(h5file)[1][9:21]
+    print(cur_time)
+
+    # Open RAMS file
+    rams_file = h5py.File(h5file, 'r')
+
+    # print variables to test code
+    print(np.shape(rams_file[var]))
+    print(len(np.shape(rams_file[var])))
+
+    # Use zoomed in region; else use entire domain
+    if x0 >= 0:
+        lats = np.array(rams_file['GLAT'][x0:x1,y0:y1])
+        lons = np.array(rams_file['GLON'][x0:x1,y0:y1])
+        topt = np.array(rams_file['TOPT'][x0:x1,y0:y1])
+        if len(np.shape(rams_file[var])) == 2:
+            pltvar = np.array(rams_file[var][x0:x1,y0:y1])
+        else:
+            pltvar = np.array(rams_file[var][zid,x0:x1,y0:y1])
+    else:
+        lats = np.array(rams_file['GLAT'][:,:])
+        lons = np.array(rams_file['GLON'][:,:])
+        topt = np.array(rams_file['TOPT'][:,:])
+        if len(np.shape(rams_file[var])) == 2:
+            pltvar = np.array(rams_file[var][:,:])
+        else:
+            pltvar = np.array(rams_file[var][zid,:,:])
+
+    # use grid points for x,y axes
+    if use_latlon == 0:
+        lons = np.arange(0,np.shape(topt)[1])
+        lats = np.arange(0,np.shape(topt)[0])
+        sname2 = 'GridPt'
+    else:
+        sname2 = 'LatLon'
+
+    fsx = 8;
+    fsy = fsx*np.shape(topt)[0]/np.shape(topt)[1]/1.2
+    # Make Figure
+    fig,ax = plt.subplots(1,1,figsize=(fsx,fsy))
+
+    a = ax.contourf(lons,lats,pltvar*scale[var],levels=clvls[var],cmap=cmaps[var],extend='both')
+    b = ax.contour(lons,lats,topt,levels=tlvls,colors='k')
+
+    plt.colorbar(a,ax=ax)
+    ax.set_title(var+' @ '+str(np.round(zcoords[zid]/1000,1))+' km:'+cur_time)
+    ax.grid()
+    plt.tight_layout()
+
+    plt.savefig(savepath+sname2+sname+'G'+gridnum+'_'+var+'_'+lvlstr+'_'+cur_time+'.png')
+    plt.close(fig)
+
+    return
 
 #### Define Functions for creating and plotting new grid ideas
 
@@ -146,78 +237,8 @@ if plt_mult_dom == 0:
        
                 znow = lvls[k] # get current altitude
                 
-                # Get RAMS vertical coodinate levels
-                zcoords = np.array(rams_tools.calc_zcoords(hefiles1[i]))
-                dzcoords = np.diff(zcoords)
-                # Get the vertical coordinate index associated with the specified altitudes
-                zid = np.where(np.abs(zcoords-znow) == np.min(np.abs(zcoords-znow)))[0][0]
-                print(zid)
-    
-                # convert altitude to string for filenames
-                lvl = int(np.array(zcoords[zid]))
-                if lvl < 10:
-                   lvlstr = '0000'+str(lvl)
-                elif lvl < 100:
-                   lvlstr = '000'+str(lvl)
-                elif lvl < 1000:
-                   lvlstr = '00'+str(lvl)
-                elif lvl < 10000:
-                   lvlstr = '0'+str(lvl)
-                else:
-                   lvlstr = str(lvl)
-    
-                # Grab current time from rams filename
-                cur_time = os.path.split(h5files1[i])[1][9:21]
-                print(cur_time)
-    
-                # Open RAMS file
-                rams_file = h5py.File(h5files1[i], 'r')
-                
-                # print variables to test code
-                print(np.shape(rams_file[var]))
-                print(len(np.shape(rams_file[var])))
-    
-                # Use zoomed in region; else use entire domain
-                if x0 >= 0:
-                   lats = np.array(rams_file['GLAT'][x0:x1,y0:y1])
-                   lons = np.array(rams_file['GLON'][x0:x1,y0:y1])
-                   topt = np.array(rams_file['TOPT'][x0:x1,y0:y1])
-                   if len(np.shape(rams_file[var])) == 2:
-                       pltvar = np.array(rams_file[var][x0:x1,y0:y1])
-                   else:
-                       pltvar = np.array(rams_file[var][zid,x0:x1,y0:y1])
-                else:
-                   lats = np.array(rams_file['GLAT'][:,:])
-                   lons = np.array(rams_file['GLON'][:,:])
-                   topt = np.array(rams_file['TOPT'][:,:])
-                   if len(np.shape(rams_file[var])) == 2:
-                       pltvar = np.array(rams_file[var][:,:])
-                   else:
-                       pltvar = np.array(rams_file[var][zid,:,:])
-    
-                # use grid points for x,y axes
-                if use_latlon == 0:
-                   lons = np.arange(0,np.shape(topt)[1])
-                   lats = np.arange(0,np.shape(topt)[0])
-                   sname2 = 'GridPt'
-                else:
-                   sname2 = 'LatLon'
-    
-                fsx = 8;
-                fsy = fsx*np.shape(topt)[0]/np.shape(topt)[1]/1.2
-                # Make Figure
-                fig,ax = plt.subplots(1,1,figsize=(fsx,fsy))
-                
-                a = ax.contourf(lons,lats,pltvar*scale[var],levels=clvls[var],cmap=cmaps[var],extend='both')
-                b = ax.contour(lons,lats,topt,levels=tlvls,colors='k')
-                
-                plt.colorbar(a,ax=ax)
-                ax.set_title(var+' @ '+str(np.round(zcoords[zid]/1000,1))+' km:'+cur_time)
-                ax.grid()
-                plt.tight_layout()
-    
-                plt.savefig(savepath+sname2+sname+'G'+gridnum+'_'+var+'_'+lvlstr+'_'+cur_time+'.png')
-                plt.close(fig)
+                # Added plot function here for parallelization                
+                plot_single_dom(var, znow, hefiles1[i], h5files1[i], x0,x1,y0,y1, scale, clvls, cmaps, tlvls, savepath, sname, gridnum)
 
 elif plt_mult_dom == 1:
 
@@ -349,10 +370,11 @@ elif plt_mult_dom == 1:
                 # Plot potential new domain
                 if plt_new_dom == 1:
                     plot_domain_boxes(ax,lat_p,lon_p,name,'green')
-    
+                    sname3 = 'AddNew_'+str(cen_lat)+'_'+str(cen_lon)+'_'+str(ny)+'_'+str(nx)+'_'
+
                 ax.set_title(var+' @ '+str(np.round(zcoords[zid]/1000,1))+' km:'+cur_time)
                 ax.grid()
                 plt.tight_layout()
     
-                plt.savefig(savepath+sname2+sname+'_MultiGrid_'+var+'_'+lvlstr+'_'+cur_time+'.png')
+                plt.savefig(savepath+sname2+sname+'_MultiGrid_'+sname3+var+'_'+lvlstr+'_'+cur_time+'.png')
                 plt.close(fig)
